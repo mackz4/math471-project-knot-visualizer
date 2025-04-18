6f05c97caaeec6d13a197bd3fd042b0c983d9c14
mackz4 <msinson@macalester.edu>
2025-04-18 14:31:02 +0100

    Added resources

---
+++ src/WaterShallow.h
@@ -0,0 +1,42 @@
+#ifndef WATER_H
+#define WATER_H
+
+#include <glm/glm.hpp>
+
+#define SIZE_X 47
+#define SIZE_Y 47
+#define TIMESTEPS 60
+
+class Water
+{
+public:
+    Water();
+    void init();
+
+    float h_list[TIMESTEPS][SIZE_X][SIZE_Y];
+private:
+    float b;
+    float g;
+    float H;
+
+    float xs[SIZE_X];
+    float ys[SIZE_Y];
+    float ts[TIMESTEPS];
+
+    float dx;
+    float dy;
+    float dt;
+
+    float x[SIZE_X][SIZE_Y];
+    float y[SIZE_X][SIZE_Y];
+
+    float v0_list[TIMESTEPS][SIZE_X][SIZE_Y];
+    float v1_list[TIMESTEPS][SIZE_X][SIZE_Y];
+
+    void linspace(float* arr, float start, float stop, int num);
+    void meshgrid(float x[SIZE_X][SIZE_Y], float y[SIZE_X][SIZE_Y], float* xs, float* ys);
+    void grad(float h_list_last_grad0[SIZE_X][SIZE_Y], float h_list_last_grad1[SIZE_X][SIZE_Y], float h_list_last[SIZE_X][SIZE_Y], float dx, float dy);
+    void div(float v_div[SIZE_X][SIZE_Y], float v0[SIZE_X][SIZE_Y], float v1[SIZE_X][SIZE_Y]);
+    void add_a_drop(float h);
+};
+#endif
